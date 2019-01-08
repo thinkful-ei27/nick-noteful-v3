0915dd83f57c15e30b61a8a1cbb5240eff960ed1
nickJ <nicknotjustin@gmail.com>
2019-01-08 16:30:19 -0600

    Routes now handles get and get by id requests. Cannot inform user of findById errors other than 500...

--- routes/notes.js
+++ routes/notes.js
@@ -1,34 +1,74 @@
 'use strict';
 
 const express = require('express');
-
+const mongoose = require('mongoose');
 const router = express.Router();
-
+const { MONGODB_URI } = require('../config');
+const Note = require('../models/note')
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  const searchTerm = req.query.searchTerm;
+  let filter = {};
+  // console.log(`${searchTerm} is the searchTerm`);
+  // console.log(`${req.query.searchTerm} is the req.searchTerm`);
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
+  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+      
+      if(searchTerm) {
+          filter.title = { $regex: searchTerm, $options: 'i' };
+      }
+
+      return Note.find(filter).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+      res.json(results);
+  })
+  .then(() => {
+      return mongoose.disconnect()
+  })
+  .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error(err);
+  });
 
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
+  const searchId = req.params.id;
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+      return Note.findById(searchId);
+  })
+  .then(results => {
+      if(results !== null){
+      res.json(results);
+      } else {
+        const err = new Error(`${searchId} not found`)
+        next(err);
+      }
+  })
+  .then(() => {
+      return mongoose.disconnect()
+  })
+  .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error(err);
+  });
 
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
+  // console.log('Get a Note');
+  // res.json({ id: 1, title: 'Temp 1' });
 
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  
+
+  // console.log('Create a Note');
+  // res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
 
 });
 
