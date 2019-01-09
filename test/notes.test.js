2624367cbf49726831be2abf635376e6dc95dc8e
nickJ <nicknotjustin@gmail.com>
2019-01-09 15:30:49 -0600

    Commented Out test/app.test.js. Added a test for GET /api/notes. Fixed a URL request problem

--- test/notes.test.js
+++ test/notes.test.js
@@ -1,21 +1,23 @@
+'use strict';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const app = require('../server');
-const { TEST_MONGODB_URI } = ('../config');
+const { TEST_MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 const { notes } = require('../db/data');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+console.log(TEST_MONGODB_URI);
 
 describe('Basic Tests for Notes Database', function(){
-  
   before(function () {
     return mongoose.connect(TEST_MONGODB_URI)
       .then(() => mongoose.connection.db.dropDatabase());
   });
 
+  console.log(TEST_MONGODB_URI);
   beforeEach(function () {
     return Note.insertMany(notes);
   });
@@ -27,5 +29,22 @@ describe('Basic Tests for Notes Database', function(){
   after(function() {
     return mongoose.disconnect();
   });
-
+//   console.log(TEST_MONGODB_URI);
+//   console.log(notes);
+  describe('GET /api/notes', function() {
+    it('Should return all the notes within the database when requested', function (){
+      let res; //can be accessed at multiple lower levels
+      return chai.request(app)
+        .get('/api/notes')
+        .then(function(_res){
+          res = _res;
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('array');
+          expect(res.body[0]).to.be.an('object');
+          expect(res.body.length).to.be.greaterThan(0);
+          expect(res.body.length).to.equal(8);
+        });  
+    });
+  });
 });
