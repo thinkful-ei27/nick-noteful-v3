56e1b3b5eea2255127b26e4f2ac1f372f483abc3
nickJ <nicknotjustin@gmail.com>
2019-01-10 13:39:59 -0600

    Added folder.js and tag.js to models folder.

---
+++ models/folder.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({});
+
+
+
+// //author : { type: mongoose.Schema.Types.ObjectId, ref: 'AuthorModel' },
+//     comments: [commentSchema]
+// });
+
+// //The below AuthorModel references the above AuthorModel
+// const Author = mongoose.model('AuthorModel', authorSchema);
---
+++ models/tag.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+const tagSchema = new mongoose.Schema({type: String});
