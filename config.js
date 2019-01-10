f77517bb7f1461fe6a649aeb18434bbb13eedb2e
nickJ <nicknotjustin@gmail.com>
2019-01-09 19:02:58 -0600

    Database now goes to mLab. Maybe.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://codern:werefox@ds153304.mlab.com:53304/notes-testing'
 };
 //noteful-test
