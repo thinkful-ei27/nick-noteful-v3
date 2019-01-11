a01c6bd87f110f36e203674c320f401aceb25b6c
nickJ <nicknotjustin@gmail.com>
2019-01-11 11:21:32 -0600

    Added a routes/tags file. server.js now references the tagsRouter and has it mounted

---
+++ routes/tags.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag')
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
 const foldersRouter = require('./routes/folders');
+const tagsRouter = require('./routes/tags');
 
 // Create an Express application
 const app = express();
@@ -26,9 +27,9 @@ app.use(express.json());
 // Mount routers: notes, folders
 app.use('/api/notes', notesRouter);
 
-app.use('/api/folders', foldersRouter)
+app.use('/api/folders', foldersRouter);
 
-
+app.use('/api/tags', tagsRouter);
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
