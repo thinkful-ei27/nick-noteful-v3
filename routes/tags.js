9336df3a737bd9fc4a65d0ceadb38b2b8aabaf15
nickJ <nicknotjustin@gmail.com>
2019-01-11 11:41:06 -0600

    Added router.get to routers/tags. Gets all or filters by searchTerm.

--- routes/tags.js
+++ routes/tags.js
@@ -7,6 +7,24 @@ const Tag = require('../models/tag')
 
 const router = express.Router();
 
+//GET Request ALL or searchTerm
+router.get('/', (req, res, next) => {
+    const { searchTerm } = req.query;
+    let filter = {};
+
+    if(searchTerm){
+        filter.name = { $regex: searchTerm, $options: 'i'};
+    }
+    
+    Tag.find(filter)
+      .sort({updatedAt: 'desc'})
+      .then((results) => {
+          res.json(results);
+      })
+      .catch((err) => {
+          next(err);
+      });
+});
 
 
 module.exports = router;
