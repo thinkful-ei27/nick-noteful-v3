a4708daf142c2fe4633b02c5c4bd2b153a5ba984
nickJ <nicknotjustin@gmail.com>
2019-01-10 22:37:13 -0600

    Uncommented out api.search(/api/folders && /api/tags in index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
 
   Promise.all([
     api.search('/api/notes'),
-    // api.search('/api/folders'),
-    // api.search('/api/tags')
+    api.search('/api/folders'),
+    api.search('/api/tags')
   ])
     .then(([
       notes,
